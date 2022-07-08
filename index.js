aae104a8f26769137077fd5a845c3ac99feb8a19
vaishnavihole <vaishnavihole1@gmail.com>
2022-07-08 14:18:31 +0530

    Port from Env

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-
-
 const express = require('express');
+require('dotenv').config()
 const app = express();
 app.use(express.json())
 
-const PORT = 5000;
+const PORT = process.env.PORT ||5000;
 
 
-// ontemp database 
+// temp database 
 let tasks = []
 
 
